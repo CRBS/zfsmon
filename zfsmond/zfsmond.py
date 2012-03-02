fffa919619cc7efea166764b0c74534989a86a19
Jenner LaFave <jlafave@ucsd.edu>
2012-03-02 15:04:02 -0800

    Added initial local script for running ZFS commands, parsing the output, and submitting it to the server.

---
+++ zfsmond/zfsmond.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+# A local script to parse output from ZFS monitoring programs and push
+# it to a ZFSmon server.
+
+import sys
+import os
+
+def main():
+    pass
+
+
+
+
+if __name__ == "__main__":
+    main()
