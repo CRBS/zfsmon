8e9f69ef6a0bb2b0f428eab1b7d8da42265d4dbd
Jenner LaFave <jlafave@ucsd.edu>
2012-03-06 16:33:49 -0800

    Added abstractzfs to version control. This implements an abstract class for ZMount and ZPool to extend.

---
+++ zfsmond/abstractzfs.py
@@ -0,0 +1,35 @@
+class AbstractZFS(object):
+""" An abstract class to represent an object containing data output from 
+    any ZFS listing/monitoring program. They should at the least contain
+    a self.properties dictionary with key-value pairs for each property
+    in the `? list -o all` output, where ? can be zfs or zpool. self.name
+    should also be defined in a constructor. Subclasses should override
+    the property_parse(properties) method to parse the properties string
+    sensibly for its type."""
+
+    def __init__(self, properties):
+        self.properties = property_parse(properties)
+        self.name = self.properties[0]
+
+    def __str__(self):
+        return self.name + " " + str(self.properties)
+
+    @staticmethod
+    def property_parse(properties):
+        pass
+
+    @staticmethod
+    def parse_size(size):
+    """ Parses the size value as output from zfs or zpool into a number of bytes."""
+        MULTIPLIERS = {'K': 10**3, 'M': 10**6, 'G': 10**9, 'T': 10**12, 'P': 10**15}
+        try:
+            sint = int(size)
+            return sint
+        except ValueError:
+            size = size.strip()
+            for m in MULTIPLIERS.iterkeys():
+                if m in size:
+                   return int( float(size[:len(size)-1]) * MULTIPLIERS[m] )
+        raise ValueError("Could not parse " + size + " as a size in bytes.")
+
+
