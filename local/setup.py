24a6dfe6178e4fa7f8a606ac5a970c4c495cfa73
Jenner LaFave <jlafave@ucsd.edu>
2012-04-09 17:32:03 -0700

    Adjusted packages structure, wrote setup script and built 2.6 egg.

---
+++ local/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup( name = 'zfsmond',
+       version = '0.1',
+       description = 'ZFS Monitoring Script',
+       author = 'Jenner LaFave',
+       author_email = 'jlafave@ucsd.edu',
+       url = 'http://crbs.ucsd.edu',
+       packages = ['zfsmond'],
+       requires = ['requests'],
+       data_files = [('/etc', ['zfsmond.conf'])]
+       )
