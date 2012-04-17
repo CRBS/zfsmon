dfdadaa15646c03de4f7b90b2c1ec5bb746ad7c1
Jenner LaFave <jlafave@ucsd.edu>
2012-04-17 15:17:08 -0700

    Updated egg to 0.1.5 to fix the duplicate filesystem updates issue.

--- local/setup.py
+++ local/setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup( name = 'zfsmond',
-       version = '0.1.4',
+       version = '0.1.5',
        description = 'ZFS Monitoring Script',
        author = 'Jenner LaFave',
        author_email = 'jlafave@ucsd.edu',
