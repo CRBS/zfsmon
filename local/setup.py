95897705239a51987eed7662eb8b62c4c7768837
Jenner LaFave <jenner@jfave.com>
2012-07-17 22:11:28 -0700

    Rebuilt egg.

--- local/setup.py
+++ local/setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup( name = 'zfsmond',
-       version = '0.2.1',
+       version = '0.2.2',
        description = 'ZFS Monitoring Script',
        author = 'Jenner LaFave',
        author_email = 'jlafave@ucsd.edu',
