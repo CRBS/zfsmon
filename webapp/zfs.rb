d2f40e4c8a383e91608b6eaf896ec76d3d48ecde
Jenner LaFave <jenner@jfave.com>
2012-07-18 17:52:36 -0700

    Performance tweaks to webapp

--- webapp/zfs.rb
+++ webapp/zfs.rb
@@ -243,6 +243,8 @@ post '/:host/datasets/:ds/snapshots/:snap/?' do
         host_not_found params[:host]
     end
     @ds = ZUtil.get_ds_record @host, params[:ds]
+    sleep 0.5 if not @ds.saved?
+
     @snap = @ds.snapshots.first_or_create :dataset => @ds, :name => params[:snap]
     request.POST.each do |k, v|
         if not ZUtil::ZFS_DATASET_FIELDS.include? k
