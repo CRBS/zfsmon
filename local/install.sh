384a526c77071ddad3b62af7a6c6371cb03ccb24
J. Brandt Buckley <brandt@runlevel1.com>
2012-07-19 19:32:19 -0700

    Swapped echo for printf due to portability
    In particular, echo -n and echo -e don't always work.
    
    For instance:
    sh-3.2# echo -n $SHELL
    -n /bin/sh
    sh-3.2# /bin/bash
    bash-3.2# echo -n $SHELL
    /bin/shbash-3.2#
    
    More info:
    http://wiki.bash-hackers.org/scripting/nonportable#echo_command
    
    Don't you love shell scripting?

--- local/install.sh
+++ local/install.sh
@@ -8,39 +8,39 @@ function install_setuptools() {
     rm -f setuptools-0.6c11-py2.6.egg
 }
 
-echo -n "Checking for easy_install... "
-which easy_install 2>/dev/null || (echo -e "not found.\nInstalling setuptools..." && install_setuptools)
-echo ""
+printf '%s' "Checking for easy_install... "
+which easy_install 2>/dev/null || (printf '%s' "not found.\nInstalling setuptools..." && install_setuptools)
+printf '\n'
 
 if [[ -f /usr/lib/python2.6/site-packages/zfsmond-latest.egg ]]; then
-    echo "zfsmond is already installed. performing upgrade..."
+    printf '%s\n' "zfsmond is already installed. performing upgrade..."
     sudo rm -rf /usr/lib/python2.6/site-packages/zfsmond-latest.egg
 fi
 
-echo "Installing latest requests library..."
+printf '%s\n' "Installing latest requests library..."
 easy_install requests >/dev/null
 
-echo -n "Installing latest zfsmond egg... "
-wget http://devilray.crbs.ucsd.edu/skip-proxy/zfsmond-latest.egg --quiet && sudo easy_install zfsmond-latest.egg >/dev/null && echo "done."
+printf '%s' "Installing latest zfsmond egg... "
+wget http://devilray.crbs.ucsd.edu/skip-proxy/zfsmond-latest.egg --quiet && sudo easy_install zfsmond-latest.egg >/dev/null && printf '%s\n' "done."
 
 if [[ ! -f /etc/zfsmond.conf ]]; then
-    echo "Getting configuration file... "
-    wget http://devilray.crbs.ucsd.edu/skip-proxy/zfsmond.conf --quiet && sudo cp zfsmond.conf /etc/ && echo "placed at /etc/zfsmond.conf"
+    printf '%s\n' "Getting configuration file... "
+    wget http://devilray.crbs.ucsd.edu/skip-proxy/zfsmond.conf --quiet && sudo cp zfsmond.conf /etc/ && printf '%s\n' "placed at /etc/zfsmond.conf"
     rm -f zfsmond.conf
 fi
 
-(which updater.py &>/dev/null && echo "updater.py is installed.") || (echo "Something went wrong during installation." && return 1)
+(which updater.py &>/dev/null && printf '%s\n' "updater.py is installed.") || (printf '%s\n' "Something went wrong during installation." && return 1)
 read -p "updater.py should be added to the crontab to run every 15 minutes. Do this now? (y/n) " -n 1
 
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-    ( crontab -l 2>/dev/null | grep -Fv updater.py ; echo "0,15,30,45 * * * * /usr/bin/updater.py 2>&1 >> /var/log/zfsmond" ) | crontab
+    ( crontab -l 2>/dev/null | grep -Fv updater.py ; printf '%s\n' "0,15,30,45 * * * * /usr/bin/updater.py 2>&1 >> /var/log/zfsmond" ) | crontab
 elif [[ $REPLY =~ ^[Nn]$ ]]; then
-    echo "Add the following line to root\'s crontab if you\'d like to do it yourself."
-    echo "0,15,30,45 * * * * /usr/bin/updater.py 2>&1 >> /var/log/zfsmond"
+    printf '%s\n' "Add the following line to root\'s crontab if you\'d like to do it yourself."
+    printf '%s\n' "0,15,30,45 * * * * /usr/bin/updater.py 2>&1 >> /var/log/zfsmond"
 else
-    echo "updater.py needs to be added to root's crontab to run every 15 minutes. Add the following line: "
-    echo "0,15,30,45 * * * * /usr/bin/updater.py 2>&1 >> /var/log/zfsmond"
+    printf '%s\n' "updater.py needs to be added to root's crontab to run every 15 minutes. Add the following line: "
+    printf '%s\n' "0,15,30,45 * * * * /usr/bin/updater.py 2>&1 >> /var/log/zfsmond"
 fi
-echo ""
+printf '\n'
 unset install_setuptools
 rm -f zfsmond-latest.egg
