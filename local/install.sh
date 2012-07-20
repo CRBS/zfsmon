01775ac80d17bc315dcce4d43328799387ff06a5
J. Brandt Buckley <brandt@runlevel1.com>
2012-07-19 20:39:02 -0700

    Changed to portable interactive (y/n) prompt
    The '-p prompt' and '-n nchars' options aren't present in some versions
    of `read`.

--- local/install.sh
+++ local/install.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
+
+ZFSMON_UPDATER_PATH='/usr/bin/updater.py'
+ZFSMON_LOG_PATH='/var/log/zfsmond'
+
+ZFSMON_CRON="*/15 * * * * $ZFSMON_UPDATER_PATH 2>&1 >> $ZFSMON_LOG_PATH"
+
 export http_proxy="http://webproxy.ucsd.edu:3128"
 export https_proxy="http://webproxy.ucsd.edu:3128"
+
 
 function install_setuptools() {
     wget http://pypi.python.org/packages/2.6/s/setuptools/setuptools-0.6c11-py2.6.egg
@@ -8,20 +15,25 @@ function install_setuptools() {
     rm -f setuptools-0.6c11-py2.6.egg
 }
 
+
 printf '%s' "Checking for easy_install... "
-which easy_install 2>/dev/null || (printf '%s' "not found.\nInstalling setuptools..." && install_setuptools)
+which easy_install 2>/dev/null || (printf '%s\n' 'easy_install not found.' 'Installing setuptools...' && install_setuptools)
 printf '\n'
+
 
 if [[ -f /usr/lib/python2.6/site-packages/zfsmond-latest.egg ]]; then
     printf '%s\n' "zfsmond is already installed. performing upgrade..."
     sudo rm -rf /usr/lib/python2.6/site-packages/zfsmond-latest.egg
 fi
 
+
 printf '%s\n' "Installing latest requests library..."
 easy_install requests >/dev/null
 
+
 printf '%s' "Installing latest zfsmond egg... "
 wget http://devilray.crbs.ucsd.edu/skip-proxy/zfsmond-latest.egg --quiet && sudo easy_install zfsmond-latest.egg >/dev/null && printf '%s\n' "done."
+
 
 if [[ ! -f /etc/zfsmond.conf ]]; then
     printf '%s\n' "Getting configuration file... "
@@ -29,18 +41,25 @@ if [[ ! -f /etc/zfsmond.conf ]]; then
     rm -f zfsmond.conf
 fi
 
-(which updater.py &>/dev/null && printf '%s\n' "updater.py is installed.") || (printf '%s\n' "Something went wrong during installation." && return 1)
-read -p "updater.py should be added to the crontab to run every 15 minutes. Do this now? (y/n) " -n 1
 
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-    ( crontab -l 2>/dev/null | grep -Fv updater.py ; printf '%s\n' "0,15,30,45 * * * * /usr/bin/updater.py 2>&1 >> /var/log/zfsmond" ) | crontab
-elif [[ $REPLY =~ ^[Nn]$ ]]; then
-    printf '%s\n' "Add the following line to root\'s crontab if you\'d like to do it yourself."
-    printf '%s\n' "0,15,30,45 * * * * /usr/bin/updater.py 2>&1 >> /var/log/zfsmond"
-else
-    printf '%s\n' "updater.py needs to be added to root's crontab to run every 15 minutes. Add the following line: "
-    printf '%s\n' "0,15,30,45 * * * * /usr/bin/updater.py 2>&1 >> /var/log/zfsmond"
-fi
+(which updater.py &>/dev/null && printf '%s\n' "updater.py is installed.") || (printf '%s\n' "Something went wrong during installation." && return 1)
+
+
+# Prompt user: Add to crontab?
+printf '%s\n' 'This script should be invoked by cron every 15 minutes.'
+while : ; do
+  printf '%s' "Add to $USER crontab? (y/n): "
+  read REPLY
+  case $REPLY in
+    [Yy]) crontab -l 2>/dev/null | grep "$ZFSMON_UPDATER_PATH" 2>&1 >/dev/null || printf '%s\n' "$ZFSMON_CRON" | crontab
+          break ;;
+    [Nn]) printf '%s\n' "Okay, then you'll have to add this line to the crontab yourself: "
+          printf '%s\n' "$ZFSMON_CRON"
+          break ;;
+       *) printf '%s\n' 'Invalid selection.' ;;
+  esac
+done
 printf '\n'
+
 unset install_setuptools
 rm -f zfsmond-latest.egg
